96f1b09bc318b5f5faaf628e24fbe96feaeaae4f
MehraDevesh2022 <mehra.devesh2022@gmail.com>
2022-08-20 17:21:12 +0530

    mvc architecture

--- Backend-Project/BackEnd/server.js
+++ Backend-Project/BackEnd/server.js
@@ -10,6 +10,12 @@ const jwt = require("jsonwebtoken");
 const secrets = require("./secret");
 const secret = require("./secret");
 const { model } = require("mongoose");
+//using Router() function for reducing route path =>
+// for authentiction routes like login signup.
+// const AuthRoute = express.Router();
+// // for user accesse part eg profile and
+// const userRoute = express.Router();
+
 // sign up =>
 /* input :
    name,
@@ -24,8 +30,46 @@ const { model } = require("mongoose");
 
 app.use(express.json());
 app.use(cookiePraser());
+
+//setting routers name. 
+
 // singnup and create document  at foodmodel
-app.post("/signup", async function (req, res) {
+app.post("/signup", signupController);
+
+// log-in input => email + password
+app.post("/login", loginController);
+
+//users data => get all user data => sensitive data => add midlleware with in it => only loged in user get that data
+app.get("/users", protectRoute, getAlluserController);
+
+// get user data =>  who is loged in current time .
+ app.get("/user", protectRoute, profileController);
+
+
+// forgot password =>
+app.patch("/forgotPassword", forgotPasswordController);
+
+// reset password using otp as an token =>
+app.patch("/restPassword", resetController);
+
+
+// otp genrator function for forgot password =>
+function generateOtp() {
+  const otp = Math.trunc(100000 + Math.random() * 900000); // six digit number 1-9
+  return otp;
+}
+
+
+
+
+
+// creating a server at port number 3000
+app.listen(3000, function () {
+  console.log("This is from Port 3000");
+});
+
+// ********************************controller function ****************************\\
+async function signupController(req, res) {
   try {
     let data = req.body;
     console.log(data);
@@ -36,10 +80,9 @@ app.post("/signup", async function (req, res) {
   } catch (err) {
     res.end(err.message);
   }
-});
+}
 
-// log-in input => email + password
-app.post("/login", async function (req, res) {
+async function loginController(req, res) {
   try {
     let data = req.body;
     let { email, password } = data;
@@ -74,23 +117,34 @@ app.post("/login", async function (req, res) {
   } catch (err) {
     res.end(err.message);
   }
-});
+}
 
-//users data => get all user data => sensitive data => add midlleware with in it => only loged in user get that data
-
-app.get("/users", protectRoute, async function (req, res) {
+async function forgotPasswordController(req, res) {
   try {
-    let users = await userModel.find();
-    // console.log(users);
-    res.json(users); // to send json data
-  } catch (error) {
+    let data = req.body;
+
+    let { email } = data;
+    // let test = await userModel.findOne(email);
+    // console.log(test);
+    let FiveMinute = Date.now() + 5 * 60 * 1000;
+    console.log(Date.now());
+    let otp = generateOtp();
+    let user = await userModel.findOneAndUpdate(
+      { email: email },
+      { otp: otp, otpExpiry: FiveMinute },
+      { new: true }
+    ); // email will find from FoodModel and update token(otp) at the document using {new: true}
+    console.log(user);
+    res.json({
+      data: user,
+      message: "otp send to your mail",
+    });
+  } catch (err) {
     res.end(err.message);
   }
-});
+}
 
-// get user data =>  who is loged in current time .
-
-app.get("/user", protectRoute, async function (req, res) {
+async function profileController(req, res) {
   try {
     // let data = req.body;
     // let { email } = data;
@@ -110,8 +164,87 @@ app.get("/user", protectRoute, async function (req, res) {
   } catch (error) {
     res.send(error.message);
   }
-});
+}
 
+async function getAlluserController(req, res) {
+  try {
+    let users = await userModel.find();
+    // console.log(users);
+    res.json(users); // to send json data
+  } catch (error) {
+    res.end(err.message);
+  }
+}
+
+async function resetController(req, res) {
+  try {
+    let data = req.body;
+    let { otp, password, ConfirmPassword, email } = data;
+
+    let user = await userModel.findOne({ email });
+    console.log(user);
+
+    // getting current time for comperison
+    const currTime = Date.now();
+    // getting otp exprie time durtaion given to user from document.
+    let givenTime = user.otpExpiry;
+
+    // check when time limit exeed then remove the otp and otpExpiry from user document.
+    if (currTime > givenTime) {
+      // expire the otp , delete from  user document
+      user.otp = undefined;
+
+      /*
+      also write like that =>
+        delete user.otp;
+       delete user.otpExpiry;
+
+      */
+      // and also otpExpiry
+      user.otpExpiry = undefined;
+      // all code above and changes occures in express server code base it will not update for data base.. for that user.save() used
+      // user.save() reponsible for update in server whatever change occures users document in api server.hence without this confirm pass and pass not matched
+      await user.save();
+      // res to user
+      res.json({
+        data: user,
+        message: "otp is Exipred please try again",
+      });
+    }
+
+    // check when time and otp is under protocol .
+    else {
+      // check otp and given otp is matched or not. it may from diifrent user otp. with under time limit
+      if (user.otp != otp) {
+        res.json({
+          message: "otp dose not match",
+        });
+      } else {
+        // find with otp and update pass and confirm pass
+        user = await userModel.findOneAndUpdate(
+          { otp }, // find
+          { password, ConfirmPassword }, // pass and confirm pass added
+          // {new : true } add new value in document
+          { runValidators: true, new: true } // default validator not working without {runValidators : true} eg: confirm pass won't match because validator not come into action with this .
+        );
+
+        // Delete key from DB  => get otp key and delete becasue there is no use of that after new password update.
+        user.otp = undefined;
+        // save all the update in to the document in to DB
+        user.otpExpiry = undefined;
+        // save the update.
+        await user.save();
+        // response to the user.
+        res.json({
+          data: user,
+          message: "Password is Succesfully Reset",
+        });
+      }
+    }
+  } catch (err) {
+    res.send(err.message);
+  }
+}
 // as middleware for checking user verification. is user loged-in or not.
 function protectRoute(req, res, next) {
   try {
@@ -142,109 +275,3 @@ function protectRoute(req, res, next) {
     }
   }
 }
-
-// forgot password =>
-
-app.patch("/forgotPassword", async function (req, res) {
-  try {
-    let data = req.body;
-
-    let { email } = data;
-    let FiveMinute = Date.now() + 5 * 60 * 1000;
-    console.log(Date.now());
-    let otp = generateOtp();
-    let user = await userModel.findOneAndUpdate(
-      { email: email },
-      { otp: otp, otpExpiry: FiveMinute },
-      { new: true }
-    ); // email will find from FoodModel and update token(otp) at the document using {new: true}
-    console.log(user);
-    res.json({
-      data: user,
-      message: "otp send to your mail",
-    });
-  } catch (err) {
-    res.end(err.message);
-  }
-});
-
-// otp genrator function for forgot password =>
-
-function generateOtp() {
-  const otp = Math.trunc(100000 + Math.random() * 900000); // six digit number 1-9
-  return otp;
-}
-
-// reset password using otp as an token =>
-
-app.patch("/restPassword", async function (req, res) {
-  try {
-    let data = req.body;
-    let { otp, password, ConfirmPassword, email } = data;
-
-    let user = userModel.findOne(email);
-
-    // getting current time for comperison
-    const currTime = Date.now();
-    // getting otp exprie time durtaion given to user from document.
-    let givenTime = user.otpExpiry;
-    // check when time limit exeed then remove the otp and otpExpiry from user document.
-    if (currTime > givenTime) {
-      // expire the otp , delete from  user document
-      user.otp = undefined;
-
-      /*
-      also write like that =>
-        delete user.otp;
-       delete user.otpExpiry;
-
-      */
-      // and also otpExpiry
-      user.otpExpiry = undefined;
-      // all code above and changes occures in express server code base it will not update for data base.. for that user.save() used
-      // user.save() reponsible for update in server whatever change occures users document in api server.hence without this confirm pass and pass not matched
-      await user.save();
-      // res to user
-      res.json({
-        data: user,
-        message: "otp is Exipred please try again",
-      });
-    }
-    // check when time and otp is under protocol .
-    else {
-      if (user.otp != otp) {
-        res.json({
-          message: "otp dose not match",
-        });
-      } else {
-        // find with otp and update pass and confirm pass
-        user = await userModel.findOneAndUpdate(
-          { otp }, // find
-          { password, ConfirmPassword }, // pass and confirm pass added
-          // {new : true } add new value in document
-          { runValidators: true, new: true } // default validator not working without {runValidators : true} eg: confirm pass won't match because validator not come into action with this .
-        );
-
-        // Delete key from DB  => get otp key and delete becasue there is no use of that after new password update.
-        user.otp = undefined;
-        // save all the update in to the document in to DB
-        user.otpExpiry = undefined;
-        // save the update.
-        await user.save();
-        // response to the user.
-        res.json({
-          data: user,
-          message: "Password is Succesfully Reset",
-        });
-      }
-    }
-    console.log(user);
-  } catch (err) {
-    res.send(err.message);
-  }
-});
-
-// creating a server at port number 3000
-app.listen(3000, function () {
-  console.log("This is from Port 3000");
-});
